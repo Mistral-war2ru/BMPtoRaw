fa791ace6c39e3ad3f0726131718565f88f9010d
Mistral-war2ru <82323152+Mistral-war2ru@users.noreply.github.com>
2022-12-11 19:52:22 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace BMPtoRaw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "bmp";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "*.bmp";
+            this.openFileDialog1.Filter = "bmp|*.bmp";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(55, 35);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace BMPtoRaw
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            if (File.Exists(openFileDialog1.FileName))
+            {
+                FileInfo bf = new FileInfo(openFileDialog1.FileName);
+                string nn = bf.Directory.FullName + @"\" + bf.Name.Remove(bf.Name.Length - 4, 4);
+                Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+                if (bmp.Palette.Entries.Length == 256)
+                {
+                    FileInfo f = new FileInfo(nn + ".pal");
+                    if (f.Exists) f.Delete();
+                    FileStream fs = f.Create();
+                    foreach (Color c in bmp.Palette.Entries)
+                    {
+                        byte[] ar = { c.R, c.G, c.B, 0 };
+                        fs.Write(ar, 0, 4);
+                    }
+                    fs.Close();
+                    FileInfo f2 = new FileInfo(nn + ".raw");
+                    if (f2.Exists) f2.Delete();
+                    FileStream fs2 = f2.Create();
+
+                    BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
+                        ImageLockMode.ReadOnly,
+                        PixelFormat.Format8bppIndexed);
+                    unsafe
+                    {
+                        byte* pointer = (byte*)(data.Scan0.ToPointer());
+                        int stride = data.Stride;
+                        int height = data.Height;
+                        byte* position;
+                        for (int i = 0; i < height; i++)
+                        {
+                            position = pointer + stride * i;
+                            for (int j = 0; j < stride; j++)
+                            {
+                                byte[] aar = { *position };
+                                fs2.Write(aar, 0, 1);
+                                position++;
+                            }
+                        }
+                    }
+                    fs2.Close();
+                }
+                else
+                    MessageBox.Show("Only 256 color bmp");
+                bmp.Dispose();
+            }
+        }
+    }
+}
